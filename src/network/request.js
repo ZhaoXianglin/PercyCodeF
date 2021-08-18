a41d702e2ddfc3406b3883d5e0338040db2dce90
Zhaoxianglin <sisivip@vip.qq.com>
2021-08-18 12:44:54 +0800

    add serve

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-        baseURL: "http://localhost:5000",
+        baseURL: "http://www.hkbu.asia:8080",
         timeout: 20000,
     })
 
